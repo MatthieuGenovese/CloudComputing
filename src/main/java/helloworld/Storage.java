f36653f751ce80498b6f193b3912a4ef07c518bc
MatthieuGenovese <gm102445@etu.unice.fr>
2017-10-13 14:29:45 +0200

    Initialisation du projet

---
+++ src/main/java/helloworld/Storage.java
@@ -0,0 +1,24 @@
+package helloworld;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Storage {
+
+    // this mocks a database.
+    private static HashMap<String, Article> contentsArticles = new HashMap<String, Article>();
+
+    public static void createArticle(String name, int price, int quantite) {
+        contentsArticles.put(price + name, new Article(name, price, quantite));
+    }
+
+
+    public static ArrayList<Article> readArticles() {
+        ArrayList<Article> results = new ArrayList<>();
+        for(Article c : contentsArticles.values()){
+                results.add(c);
+        }
+        return results;
+    }
+
+
+}
