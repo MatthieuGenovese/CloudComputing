fbff8333c8b3d5f5e3eeb6bf32b36a580ab88d4a
MatthieuGenovese <gm102445@etu.unice.fr>
2017-10-27 16:10:53 +0200

    Mise en place du datastore distant article

--- src/main/java/helloworld/DatastoreServlet.java
+++ src/main/java/helloworld/DatastoreServlet.java
@@ -1,19 +1,14 @@
 package helloworld;
 
-import com.google.cloud.Timestamp;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.cloud.datastore.*;
+import com.google.appengine.api.datastore.Entity;
 import com.google.gson.*;
-import org.json.HTTP;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.Inet4Address;
-import java.net.Inet6Address;
-import java.net.InetAddress;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -28,43 +23,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "datastore", value = "")
 public class DatastoreServlet extends HttpServlet {
 
-    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-    KeyFactory keyFactory = datastore.newKeyFactory().setKind("article");
-    IncompleteKey key = keyFactory.setKind("article").newKey();
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,
-            ServletException {
-        // store only the first two octets of a users ip address
-        String userIp = req.getRemoteAddr();
-        InetAddress address = InetAddress.getByName(userIp);
-        if (address instanceof Inet6Address) {
-            // nest indexOf calls to find the second occurrence of a character in a string
-            // an alternative is to use Apache Commons Lang: StringUtils.ordinalIndexOf()
-            userIp = userIp.substring(0, userIp.indexOf(":", userIp.indexOf(":") + 1)) + ":*:*:*:*:*:*";
-        } else if (address instanceof Inet4Address) {
-            userIp = userIp.substring(0, userIp.indexOf(".", userIp.indexOf(".") + 1)) + ".*.*";
-        }
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
-
-
-        // Record a visit to the datastore, storing the IP and timestamp.
-        FullEntity<IncompleteKey> curVisit = FullEntity.newBuilder(key)
-                .set("user_ip", userIp).set("timestamp", Timestamp.now()).build();
-        datastore.add(curVisit);
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("article");
+        IncompleteKey key = keyFactory.setKind("article").newKey();
 
         // Retrieve the last 10 visits from the datastore, ordered by timestamp.
-        EntityQuery query = Query.newEntityQueryBuilder().setKind("visit")
-                .setOrderBy(StructuredQuery.OrderBy.desc("timestamp")).setLimit(10).build();
+        EntityQuery query = Query.newEntityQueryBuilder().setKind("article").build();
         QueryResults<com.google.cloud.datastore.Entity> results = datastore.run(query);
 
         resp.setContentType("text/plain");
         PrintWriter out = resp.getWriter();
-        out.print("Last 10 visits:\n");
         while (results.hasNext()) {
-            Entity entity = results.next();
-            out.format("Time: %s Addr: %s\n", entity.getTimestamp("timestamp"),
-                    entity.getString("user_ip"));
+            com.google.cloud.datastore.Entity entity = results.next();
+            out.print("Article : " + entity.getString("nom") + " prix :" + entity.getLong("prix") + " quantite : " + entity.getLong("quantite") + "\n");
         }
     }
 
@@ -92,9 +67,13 @@ public class DatastoreServlet extends HttpServlet {
 //            gson.toJson(jb.toString());
             Article a = new Article(jsontest.get("nom").getAsString(), Integer.valueOf(jsontest.get("prix").getAsString()), Integer.valueOf(jsontest.get("quantite").getAsString()));
 //            datastore
-            
-            FullEntity<Article> list = FullEntity.newBuilder();
-            Entity entity = new Entity();
+
+            Entity entity = new Entity("article");
+            entity.setProperty("nom", a.getNom());
+            entity.setProperty("prix", a.getPrix());
+            entity.setProperty("quantite", a.getQuantite());
+            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+            datastore.put(entity);
 
 //            PrintWriter out = resp.getWriter();
             out.println(a);
