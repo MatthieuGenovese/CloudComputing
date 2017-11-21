caffdb6fa0464d4314ebc2bdf3bfc73ee1bfaad9
MatthieuGenovese <gm102445@etu.unice.fr>
2017-11-21 21:29:37 +0100

    suppression des trucs inutiles

--- src/main/java/helloworld/ArticleQueue.java
+++ src/main/java/helloworld/ArticleQueue.java
@@ -21,7 +21,7 @@ public class ArticleQueue extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         final GsonBuilder builder = new GsonBuilder();
         final Gson gson = builder.create();
-        Article a1 = new Article("a1", 10, 125);
+        /*Article a1 = new Article("a1", 10, 125);
         Article a2 = new Article("a2", 154, 45);
         Article a3 = new Article("a3", 1545, 48);
         ArrayList<Article> l = new ArrayList<>();
@@ -31,7 +31,7 @@ public class ArticleQueue extends HttpServlet {
         String res = "";
         for(Article a : l){
             res += gson.toJson(a);
-        }
+        }*/
         PrintWriter out = resp.getWriter();
         Queue queue = QueueFactory.getDefaultQueue();
         queue.add(TaskOptions.Builder.withUrl("/worker").param("key", req.getQueryString()));
